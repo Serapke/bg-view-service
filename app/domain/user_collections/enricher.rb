4f780ec6e3b97ca8d0cc1221c0e6ab665406a515
mserapinas <m.serapinas@gmail.com>
2025-11-24 07:59:22 +0200

    Refactor ViewsController by moving domain logic out to enricher and serializer

---
+++ app/domain/user_collections/enricher.rb
@@ -0,0 +1,47 @@
+module UserCollections
+  class Enricher
+    def initialize(user_id)
+      @user_id = user_id
+    end
+
+    def call
+      user_collection = fetch_user_collection
+      game_ids = extract_game_ids(user_collection)
+      games = fetch_games(game_ids)
+      enrich_collection(user_collection, games)
+    end
+
+    private
+
+    attr_reader :user_id
+
+    def fetch_user_collection
+      UserService.get_user_collection(user_id)
+    end
+
+    def extract_game_ids(user_collection)
+      user_collection.dig('games')&.map { |item| item['gameId'] }&.compact || []
+    end
+
+    def fetch_games(game_ids)
+      return [] if game_ids.empty?
+
+      GameDiscoveryService.get_games_by_ids(game_ids)
+    end
+
+    def enrich_collection(user_collection, games)
+      collection_items = user_collection.dig('games') || []
+
+      collection_items.map do |collection_item|
+        game = find_game_by_id(games, collection_item['gameId'])
+        next unless game
+
+        { collection_item: collection_item, game: game }
+      end.compact
+    end
+
+    def find_game_by_id(games, game_id)
+      games.find { |g| g['id'] == game_id }
+    end
+  end
+end
---
+++ app/domain/user_collections/serializer.rb
@@ -0,0 +1,26 @@
+module UserCollections
+  class Serializer
+    def self.serialize_collection(user_id, enriched_items)
+      new.serialize_collection(user_id, enriched_items)
+    end
+
+    def serialize_collection(user_id, enriched_items)
+      {
+        user_id: user_id,
+        collection: enriched_items.map { |item| serialize_item(item[:collection_item], item[:game]) },
+        total_games: enriched_items.size
+      }
+    end
+
+    def serialize_item(collection_item, game)
+      {
+        id: game['id'],
+        name: game['name'],
+        rating: game['rating'],
+        notes: collection_item['notes'],
+        labels: collection_item['labels'] || [],
+        modified_at: collection_item['modifiedAt']
+      }
+    end
+  end
+end
